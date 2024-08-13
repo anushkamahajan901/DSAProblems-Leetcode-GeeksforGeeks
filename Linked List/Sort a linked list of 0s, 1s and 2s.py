2a2551e9382de2716e000901fe505f132f33db3e
Anushka Ajay Mahajan <85194446+anushkamahajan901@users.noreply.github.com>
2024-08-13 20:14:33 +0530

    Create Sort a linked list of 0s, 1s and 2s.py

---
+++ Linked List/Sort a linked list of 0s, 1s and 2s.py
@@ -0,0 +1,23 @@
+# A linked list node
+class Node:
+    def __init__(self, new_data):
+        self.data = new_data
+        self.next = None
+
+# Function to sort a linked list of 0s, 1s and 2s
+def sort_list(head):
+    cnt = [0, 0, 0]
+    ptr = head
+    while ptr is not None:
+        cnt[ptr.data] += 1
+        ptr = ptr.next
+
+    idx = 0
+    ptr = head
+    while ptr is not None:
+        if cnt[idx] == 0:
+            idx += 1
+        else:
+            ptr.data = idx
+            cnt[idx] -= 1
+            ptr = ptr.next
