cafd2f3b104000fe92e033858dffc69f96138cd5
Anushka Ajay Mahajan <85194446+anushkamahajan901@users.noreply.github.com>
2024-08-19 22:21:01 +0530

    Update Second Largest number in an array GFG.py

--- Array/Second Largest number in an array GFG.py
+++ Array/Second Largest number in an array GFG.py
@@ -1,1 +1,17 @@
 
+class Solution:
+    def print2largest(self, arr):
+        # Code Here
+        largest=second_largest=-1;
+
+        for i in arr:
+            if i > largest:
+                second_largest = largest;
+                largest = i;
+
+                
+            elif i > second_largest and i!= largest:
+                second_largest = i;
+                
+        return second_largest
+                
