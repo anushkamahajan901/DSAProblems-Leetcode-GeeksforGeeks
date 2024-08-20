1c72d269adc85036ba6404d8b3bdec08d3df88d7
Anushka Ajay Mahajan <85194446+anushkamahajan901@users.noreply.github.com>
2024-08-20 23:16:16 +0530

    Update 628. Maximum Product of Three Numbers.py

--- Mathematical/628. Maximum Product of Three Numbers.py
+++ Mathematical/628. Maximum Product of Three Numbers.py
@@ -1,1 +1,5 @@
-
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        a = heapq.nlargest(3, nums)
+        b = heapq.nsmallest(2, nums)
+        return max(a[0] * a[1] * a[2], a[0] * b[0] * b[1])
